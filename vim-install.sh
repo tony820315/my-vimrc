f2f39f408da19d55712b517a4a7d032842bda055
root <jerri0912@gmail.com>
2020-07-31 00:27:18 +0800

    update vim-install

--- vim-install.sh
+++ vim-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 apt-get install exuberant-ctags ncurses-term python-jinja2 git cscope
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vim-script /usr/bin/
 cp .vimrc ~/
